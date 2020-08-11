4cd00cb981a4c07bc8efaab6f60de5ef143783f6
Brittany Joiner <brittanyjoiner15@gmail.com>
2020-08-11 16:38:54 -0500

    final edits

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var updateMarketValue = function (ele) {
-  var sharesOwned = parseFloat($(ele).children(".shares").text());
-  var marketPrice = parseFloat($(ele).children(".marketPrice").text());
+  var sharesOwned = parseFloat($(ele).find(".shares input").val());
+  var marketPrice = parseFloat($(ele).find(".marketPrice input").val());
 
   var marketValue = sharesOwned * marketPrice;
   $(ele).children(".marketValue").html(marketValue);
@@ -9,8 +9,8 @@ var updateMarketValue = function (ele) {
 };
 
 var updateProfit = function (ele, marketValue) {
-  var sharesOwned = parseFloat($(ele).children(".shares").text());
-  var costPerShare = parseFloat($(ele).children(".cost").text());
+  var sharesOwned = parseFloat($(ele).find(".shares input").val());
+  var costPerShare = parseFloat($(ele).find(".cost input").val());
   var costofPurchase = sharesOwned * costPerShare;
 
   var profit = marketValue - costofPurchase;
@@ -41,4 +41,54 @@ var updatePortfolioValueAndProfit = function () {
 
 $(document).ready(function () {
   updatePortfolioValueAndProfit();
+
+  var remove = $(document).on("click", ".btn.remove", function (event) {
+    $(this).closest("tr").remove();
+    updatePortfolioValueAndProfit();
+  });
+
+  var timeout;
+
+  $(document).on("input", "tr input", function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      updatePortfolioValueAndProfit();
+    }, 1000);
+  });
+
+  $("#addStock").on("submit", function (event) {
+    event.preventDefault();
+    var name = $(this).children("[name=name]").val();
+    var shares = $(this).children("[name=shares]").val();
+    var cost = $(this).children("[name=cost]").val();
+    var marketPrice = $(this).children("[name=price]").val();
+    console.log(name, shares, cost, marketPrice);
+
+    $("tbody").append(
+      "<tr>" +
+        '<td class="name">' +
+        name +
+        "</td>" +
+        '<td class="shares"><input type="number" value="' +
+        shares +
+        '" /></td>' +
+        '<td class="cost"><input type="number" value="' +
+        cost +
+        '" /></td>' +
+        '<td class="marketPrice"><input type="number" value="' +
+        marketPrice +
+        '" /></td>' +
+        '<td class="marketValue"></td>' +
+        '<td class="profit"></td>' +
+        '<td><button class="btn btn-light btn-sm remove">Remove</button></td>' +
+        "</tr>"
+    );
+
+    updatePortfolioValueAndProfit();
+
+    $(this).children("[name=name]").val();
+    $(this).children("[name=shares]").val();
+    $(this).children("[name=cost]").val();
+    $(this).children("[name=price]").val();
+  });
 });
